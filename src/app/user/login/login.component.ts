bedca38a51476d35ebee06975b6a9aebd0502643
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-04-04 10:48:03 +0200

    Add view password behavior

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public form: FormGroup = new FormGroup({})
+  public showPassword: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    const loginControl: AbstractControl = new FormControl('', [Validators.required])
+    const passwordControl: AbstractControl = new FormControl('', [Validators.required])
+
+    this.form.addControl('login', loginControl)
+    this.form.addControl('password', passwordControl)
+  }
+
+  passwordToggle(): void {
+    this.showPassword = !this.showPassword
+    if (this.showPassword) {
+      setTimeout(() => this.showPassword = false, 800)
+    }
   }
 
 }

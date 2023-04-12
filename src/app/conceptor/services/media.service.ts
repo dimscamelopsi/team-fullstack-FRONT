85456d97425eee995065fd4e06a11c8a2d2a8540
redg34 <98891221+redg34@users.noreply.github.com>
2023-04-12 15:01:46 +0200

    endpoint service

--- src/app/conceptor/services/media.service.ts
+++ src/app/conceptor/services/media.service.ts
@@ -1,11 +1,20 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MediaType } from "src/app/course/types/media-type";
+import { environment } from "src/environments/environment";
 
 
 @Injectable({
   providedIn:'root'
 })
 
+
 export class MediaService {
+  private readonly endpoint: string =`${environment.apiRootUri}media`
+
+constructor(
+  private _httpClient: HttpClient
+) { }
 
 }
+

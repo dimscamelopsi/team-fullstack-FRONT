c1aed58d8d760a6c2dcac19510a675481b5ed067
redg34 <98891221+redg34@users.noreply.github.com>
2023-04-12 18:31:31 +0200

    second test

--- src/app/conceptor/add-media/add-media.component.ts
+++ src/app/conceptor/add-media/add-media.component.ts
@@ -3,6 +3,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { take } from 'rxjs/operators';
+import { HttpResponse } from '@angular/common/http';
+import { FormCourseBuilderService } from 'src/app/course/services/course-handler/form-course-builder.service';
 
 
 @Component({
@@ -14,17 +17,48 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AddMediaComponent implements OnInit {
 
-  public form!: MatFormFieldModule;
+  public form!: FormGroup;
   public mediaFormGroup!: FormGroup;
   public formModule!: FormsModule;
   public router!: Router;
+  mediaService: any;
+  _toastService: any;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormCourseBuilderService
+    ) {
+      this.mediaFormGroup = this.formBuilder.form
+     }
 
   ngOnInit(): void {
-    this.mediaFormGroup = this.formBuilder.group({
-    });
+
 }
+
+public addMedia() {
+  this.mediaService.add(this.mediaFormGroup.value)
+  .pipe(
+    take(1)
+  ).suscribe({
+    next: (_Response: HttpResponse<any>) => {
+      const message: string = `Media was added.`
+      this._toastService.show(message)
+    },
+    error: (_error: any) => {
+      const badMessage: string = `Media not added !!`
+      this._toastService.show(badMessage)
+    }
+  })
+}
+/*this.mediaFormGroup = this.formBuilder.group({
+  id: null,
+  title: null,
+  summary: null,
+  duration: null,
+  totalTime: null,
+  createdAt: Date,
+  url:null,
+  typeMedia: null
+});*/
 
 onSubmitForm() {
   console.log(this.mediaFormGroup.value);
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';

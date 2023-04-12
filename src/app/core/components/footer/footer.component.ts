904ac23ab417998aec5143ce239864489c805c60
lionj <judin.malivert@poe.aelion.biz>
2023-04-12 14:18:16 +0200

    implement footer component dynamicly

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  public version: string = environment.version;
+  public annee: any = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,21 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import packageInfo from 'package.json';
 
 import { UserService } from "src/app/user/services/user.service";
 
 export const environment = {
   
   production: false,
+  version: packageInfo.version,
   apiRootUri: 'http://127.0.0.1:5000/api/v1/',
   storage: {
     auth: {
       strategy: 'session',
       key: 'auth',
-    }
-  }
+    },
+  },
 };
 
 /*

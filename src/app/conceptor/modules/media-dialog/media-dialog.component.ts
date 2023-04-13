a132768debaba093fc28a738c87a5c202b7d6ee3
boudi1983 <boudi.mohand@gmail.com>
2023-04-13 15:05:36 +0200

    Add method findAllMedia

--- src/app/conceptor/modules/media-dialog/media-dialog.component.ts
+++ src/app/conceptor/modules/media-dialog/media-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaService } from '../../services/media.service';
+import { MediaType } from 'src/app/course/types/media-type';
+import { take } from 'rxjs';
+import { ModuleType } from '../../types/module-type';
 
 @Component({
   selector: 'app-media-dialog',
@@ -6,8 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-dialog.component.scss']
 })
 export class MediaDialogComponent implements OnInit {
+  public medias: Array<MediaType> = []
 
-  constructor() { }
+  constructor(
+    private _mediaService: MediaService
+  ) { }
 
   ngOnInit(): void {
   }

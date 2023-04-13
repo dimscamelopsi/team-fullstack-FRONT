02419a59b341d9b91c52dd732f8614c1022d977c
pelty <daniel.landally@hotmail.fr>
2023-04-13 16:02:36 +0200

    create dialog updateCourseManage

--- src/app/course/components/manage-course/manage-course.component.ts
+++ src/app/course/components/manage-course/manage-course.component.ts
@@ -3,9 +3,15 @@ import { CourseListType } from '../../types/course-list-type';
 import { CourseService } from '../../services/course.service';
 import { ToastService } from 'src/app/core/toast.service';
 import { take } from 'rxjs';
-import { HttpResponse } from '@angular/common/http';
 import { ModuleType } from '../../types/module-type';
 import { CourseManageType } from '../../types/course-manage-type';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateCourseManageComponent } from '../../dialogs/update-course-manage/update-course-manage.component';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-manage-course',
@@ -13,18 +19,30 @@ import { CourseManageType } from '../../types/course-manage-type';
   styleUrls: ['./manage-course.component.scss']
 })
 export class ManageCourseComponent implements OnInit {
+  animal!: string
+  name: string = 'Daniel'
 
   private _visibility!: boolean
   public courses: Array<CourseManageType> = []
   
   constructor(
-    private _courseService: CourseService,
-    private _toastService: ToastService) { }
+    public dialog: MatDialog,
+    private _courseService: CourseService) { }
 
   ngOnInit(): void {
     this._courseService.findListCourse()
       .pipe(take(1)).subscribe(
         (response: CourseManageType[]) => {this.courses = response })
+  }
+
+  openDialog(course: CourseManageType): void {
+    const dialogRef = this.dialog.open(UpdateCourseManageComponent, {
+      data: {name: course.title, animal: this.animal},
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.animal = result;
+    })
   }
 
   onCourseToggle(course: CourseListType): void {

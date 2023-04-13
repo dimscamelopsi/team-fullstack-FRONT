62d2d0b73c498f0e0b70bff484da77bfd983b3e7
pelty <daniel.landally@hotmail.fr>
2023-04-13 09:52:36 +0200

    create component Manage-course

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -9,6 +9,7 @@ import { ModuleListComponent } from './components/module-list/module-list.compon
 import { RemoveCourseDialogComponent } from './dialogs/remove-course-dialog/remove-course-dialog.component';
 import { CourseHandlerComponent } from './course-handler/course-handler.component';
 import { ModuleAddComponent } from './dialogs/module-add/module-add.component';
+import { ManageCourseComponent } from './components/manage-course/manage-course.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ModuleAddComponent } from './dialogs/module-add/module-add.component';
     ModuleListComponent,
     RemoveCourseDialogComponent,
     CourseHandlerComponent,
-    ModuleAddComponent
+    ModuleAddComponent,
+    ManageCourseComponent
   ],
   imports: [
     SharedModule,

418c1b358b9752ee2e9eb93ab98ac60a0e0d33d8
dimscamelopsi <dimitri.couchot@hotmail.fr>
2023-04-13 10:31:37 +0200

    what are those files ????

---
+++ src/app/conceptor/conceptor-routing.module_REMOTE_912.ts
@@ -0,0 +1,31 @@
+import { Component, NgModule } from '@angular/core';
+import { OutletContext, RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ListComponent } from '../course/list/list.component';
+import { CourseHandlerComponent } from '../course/course-handler/course-handler.component';
+
+
+
+@NgModule({
+  imports: [RouterModule.forChild(ConceptorRoutingModule.routes)],
+  exports: [RouterModule]
+})
+export class ConceptorRoutingModule {
+  public static routes: Routes = [
+    {
+      path: '',
+      component: SidebarComponent, children:[
+        {
+        path: 'addCourse',
+       component: CourseHandlerComponent
+        },
+        {
+          path:'list',
+          component:ListComponent
+        }
+
+      ]
+    }
+
+  ];
+ }

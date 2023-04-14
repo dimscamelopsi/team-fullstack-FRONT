89e13de5941a36632f8b2c68cbfe7f14e831941f
pelty <daniel.landally@hotmail.fr>
2023-04-14 12:30:44 +0200

    refactor course Handler

--- src/app/course/dialogs/update-course-manage/update-course-manage.component.ts
+++ src/app/course/dialogs/update-course-manage/update-course-manage.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DialogData } from '../../components/manage-course/manage-course.component';
+import { CourseManageType } from '../../types/course-manage-type';
+import { CourseModel } from '../../models/course-model';
+import { Observable, map } from 'rxjs';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-update-course-manage',
@@ -8,11 +12,12 @@ import { DialogData } from '../../components/manage-course/manage-course.compone
   styleUrls: ['./update-course-manage.component.scss']
 })
 export class UpdateCourseManageComponent implements OnInit {
+  private _course: CourseModel = new CourseModel()
+  private _form: FormGroup = new FormGroup({})
 
   constructor(
     public dialogRef: MatDialogRef<UpdateCourseManageComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: CourseManageType ) { }
 
   ngOnInit(): void {
   }
@@ -20,5 +25,9 @@ export class UpdateCourseManageComponent implements OnInit {
   onNoClick(): void {
     this.dialogRef.close();
   }
+  public get c(): {[key: string]: AbstractControl} {
+    return this._form.controls
+  }
+
 
 }

b696f1d93c54e2f31dbcd689be8650e96a5ae1f3
boudi1983 <boudi.mohand@gmail.com>
2023-04-17 08:44:06 +0200

    changed module type on module add type

--- src/app/course/types/module-add-type.ts
+++ src/app/course/types/module-add-type.ts
@@ -1,14 +1,10 @@
 import { CourseType } from "./course-type"
 import { MediaType } from "./media-type"
 
-export type ModuleType = {
+export type ModuleAddType = {
   id?: number
   name: string
   objective: string
-  selected: boolean
-  totalTime?: string
   course: CourseType
-  medias: MediaType[]
-  
-
+  media: MediaType[]
 }

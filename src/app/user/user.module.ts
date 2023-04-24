ca9dc392e7b5cd7b052728d69f20693300e36e92
boudi1983 <boudi.mohand@gmail.com>
2023-04-24 19:18:29 +0200

    Stack for forgotten password

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { ForgottenPasswordComponent } from './forgotten-password/forgotten-password.component';
+import { NewPasswordComponent } from './new-password/new-password.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ForgottenPasswordComponent,
+    NewPasswordComponent
   ],
   imports: [
     CommonModule,

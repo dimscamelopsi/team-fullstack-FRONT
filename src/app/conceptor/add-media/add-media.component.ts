99394b200b3b8cdbab5d17e346a120634799e257
redg34 <98891221+redg34@users.noreply.github.com>
2023-04-28 18:22:58 +0200

    erreur apres back

--- src/app/conceptor/add-media/add-media.component.ts
+++ src/app/conceptor/add-media/add-media.component.ts
@@ -1,16 +1,19 @@
-import { Component, Directive, OnInit , ViewChild, ElementRef} from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { ToastService } from 'src/app/core/toast.service';
-import { Observable, take } from 'rxjs';
+import { take } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
 import { MediaType } from 'src/app/course/types/media-type';
 import { AddMediaType } from 'src/app/course/types/add-media-type';
+
 import { ModuleListType } from 'src/app/course/types/media-list-type';
 import { TypeMediaType } from 'src/app/course/types/type-media-type';
+
+import { log } from 'console';
 import { MediaService } from '../modules/services/media.service';
 import { ModuleService } from '../modules/services/module.service';
 import { TypeMediaService } from '../modules/services/type-media.service';
-
 
 
 
@@ -20,8 +23,6 @@ import { TypeMediaService } from '../modules/services/type-media.service';
   styleUrls: ['./add-media.component.scss']
 })
 export class AddMediaComponent implements OnInit {
-  @ViewChild("fileDropRef", { static: false }) fileDropEl!: ElementRef;
-  public files: any[] = [];
   mediaFormGroup!: FormGroup;
   public mediaType!: MediaType[];
   public addMediaType!: AddMediaType[];
@@ -30,16 +31,11 @@ export class AddMediaComponent implements OnInit {
   selectedOption = this.typeMedias[0];
   selectedFiles: File[] = [];
   mediaForm: any;
-  selectedType: string = '';
-  fileOver: boolean = false;
-  formData = new FormData();
-
-
 
 
   constructor(
     private _fb: FormBuilder,
-    private mediaService: MediaService,
+    private _mediaService: MediaService,
     private _toastService: ToastService,
     private _moduleService: ModuleService,
     private _typeMediaService: TypeMediaService,
@@ -94,8 +90,8 @@ export class AddMediaComponent implements OnInit {
 
   }
   public addMedia() {
-/*  console.log(this.mediaFormGroup.value)
-    this.mediaService.add(this.mediaFormGroup.value)
+/* console.log(this.mediaFormGroup.value) */
+    this._mediaService.add(this.mediaFormGroup.value)
       .pipe(
         take(1)
       ).subscribe({
@@ -108,109 +104,44 @@ export class AddMediaComponent implements OnInit {
           const badMessage: string = `media not added.`
           this._toastService.show(badMessage)
         }
-      }) */
+      })
   }
 
 
 
-  onSubmit(): void {
-    // Créez un objet FormData pour envoyer les données vers le serveur
-    const formData = new FormData();
 
-    // Ajoutez chaque fichier sélectionné à l'objet FormData
-    for (let i = 0; i < this.selectedFiles.length; i++) {
-      formData.append('file[]', this.selectedFiles[i], this.selectedFiles[i].name);
-    }
-
-    // Réinitialisez le formulaire
-    this.mediaFormGroup.reset();
-
-    // Réinitialisez la liste des fichiers sélectionnés
-    this.selectedFiles = [];
-
-/*     // Appelez une méthode de service pour envoyer les données vers le serveur
-    this.mediaService.add(formData).subscribe(
-      (response) => {
-        // Traitez la réponse du serveur
-      },
-      (error) => {
-        // Traitez l'erreur
-      }
-    ); */
+onSubmit(): void {
+  const formData = new FormData();
+  for (let i = 0; i < this.selectedFiles.length; i++) {
+    formData.append('file[]', this.selectedFiles[i], this.selectedFiles[i].name);
   }
+  this.mediaFormGroup.reset();
+  this.selectedFiles = [];
+}
 
+   onFileSelected(event: any): void {
+     for (let i = 0; i < event.target.files.length; i++) {
+       this.selectedFiles.push(event.target.files[i]);
+     }
+   }
 
+   onFileDropped(event: any): void {
+     event.preventDefault();
+     for (let i = 0; i < event.dataTransfer.files.length; i++) {
+       this.selectedFiles.push(event.dataTransfer.files[i]);
+     }
+   }
 
-  /**
-   * handle file from browsing
-   */
-  fileBrowseHandler(files:any) {
-    this.prepareFilesList(files);
-  }
+   onDragOver(event: any): void {
+     event.preventDefault();
+   }
 
-  /**
-   * Delete file from files list
-   * @param index (File index)
-   */
-  deleteFile(index: number) {
-    if (this.files[index].progress < 100) {
-      console.log("Upload in progress.");
-      return;
-    }
-    this.files.splice(index, 1);
-  }
+   uploadFile(evt: any){
+     // evt est un tableau des fichier(s) déposés sur notre div. Ici nous supposerons qu'il y a un seul fichier uploadé
 
-  /**
-   * Simulate the upload process
-   */
-  uploadFilesSimulator(index: number) {
-    setTimeout(() => {
-      if (index === this.files.length) {
-        return;
-      } else {
-        const progressInterval = setInterval(() => {
-          if (this.files[index].progress === 100) {
-            clearInterval(progressInterval);
-            this.uploadFilesSimulator(index + 1);
-          } else {
-            this.files[index].progress += 5;
-          }
-        }, 200);
-      }
-    }, 1000);
-  }
-
-  /**
-   * Convert Files list to normal array list
-   * @param files (Files List)
-   */
-  prepareFilesList(files: Array<any>) {
-    for (const item of files) {
-      item.progress = 0;
-      this.files.push(item);
-    }
-    this.fileDropEl.nativeElement.value = "";
-    this.uploadFilesSimulator(0);
-  }
-
-
-
-  onFileSelect(event: any, type: string) {
-    this.selectedType = type;
-    if (this.selectedType === 'slide' || this.selectedType === 'document') {
-      this.fileOver = true;
-    }
-    const files = event.target.files;
-
-  }onFileSelected(event: any) {
-    const files: FileList = event.target.files;
-    for (let i = 0; i < files.length; i++) {
-      this.uploadFile(files[i]);
-    }
-  }
-  uploadFile(arg0: File) {
-    throw new Error('Method not implemented.');
-  }
-
+       let payload = new FormData();
+       payload.append('data', evt[0]);
+       // Nous pouvons maintenant uploader le fichier en lancant une requete POST avec la variable payload comme corps de requete :)
+     }
 
 }

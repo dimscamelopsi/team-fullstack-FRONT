8610c0fa62cecab625fb3b5bacb2b5aa5a9043e2
boudi1983 <boudi.mohand@gmail.com>
2023-05-03 19:10:06 +0200

    Add test cyPress for module and login

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,22 @@
+describe('Connect to different dashboard', () => {
+    beforeEach(() => {
+      cy.viewport(1280, 900);
+      cy.visit('user/login');
+    });
+  
+    it('Should connecte and disconnect from the dashboard', () => {
+      cy.get('form').within(($form) => {
+        cy.get('input[formControlName="login"]').type('Lemaire.Aldegonde');
+        cy.get('input[formControlName="password"]').type('Java@12345');
+          cy.root().submit();
+      });
+        
+    });
+    it('Test connect if your login and password without', () => {
+        cy.get('form').within(($form) => {
+          cy.get('input[formControlName="login"]').type('1111');
+          cy.get('input[formControlName="password"]').type('1111');
+          cy.root().submit();
+        });
+      });
+  });
